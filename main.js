e7f4d59c2216c5ded53afaa9b958dc7462c983b0
aFish <tako0903@icloud.com>
2019-01-11 19:22:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function appendImage () {
-    $.getJSON("http://178.128.61.189:5000/times/10", function (data) {
+    $.getJSON("http://yiyu0x.tk:5000/times/10", function (data) {
         $.each(data, function (key, val) {
             $('#gallery').append('<a>' +
                 '<img src="' + val + '" />' +
